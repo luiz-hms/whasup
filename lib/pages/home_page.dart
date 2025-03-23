c41b800671940d4752c6b51b8b938f76c5088763
luiz-hms <luiz.henriquems@hotmail.com>
2025-03-23 14:07:13 -0300

    tabs
    tabs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,14 +7,45 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        bottom: TabBar(
+          indicatorWeight: 4,
+          labelStyle: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: [
+            Tab(
+              text: "Conversar",
+            ),
+            Tab(
+              text: "Contatos",
+            ),
+          ],
+        ),
         title: const Text('whasupp'),
       ),
-      body: Container(),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Text("Conversar"),
+          Text("Contatos"),
+        ],
+      ),
     );
   }
 }
